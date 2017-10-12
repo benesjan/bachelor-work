dce1a4d901467709f7a45d5a96c7bad4fdd17015
Jan Beneš <janbenes1234@gmail.com>
2017-10-12 18:28:31 +0200

    Classifier learning

--- clasification_learning.py
+++ clasification_learning.py
@@ -1,16 +1,20 @@
+import numpy as np
 import pickle
-from sys import exit
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
 
 import config
 from data_utils import load_sparse_csr
 
 if __name__ == '__main__':
-    try:
-        matrix = load_sparse_csr(config.data_matrix_path)
+    X = load_sparse_csr(config.data_matrix_path)
+    Y = np.load(config.topics_matrix_path)
 
-        with open(config.topics_matrix_path, 'rb') as f:
-            topics = pickle.load(f)
+    # The process of vectorizing and learning could be streamlined by using Pipeline
+    clf = OneVsRestClassifier(LinearSVC())
+    print("Training the classifier")
+    clf.fit(X, Y)
 
-    except FileNotFoundError as e:
-        print("File loading failed: " + e.filename)
-        exit(1)
+    print("Saving the classifier to file")
+    with open(config.classifier_path, 'wb') as handle:
+        pickle.dump(clf, handle, protocol=pickle.HIGHEST_PROTOCOL)
--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 data_directory = './data'
 
 training_data_path = data_directory + '/CNO_IPTC_train.txt'
-# training_data_path = data_directory + '/test1'
 data_matrix_path = data_directory + '/matrix.npz'
 topics_matrix_path = data_directory + '/topics.npy'
 
 data_vectorizer_path = data_directory + '/vectorizer.pickle'
 topic_binarizer_path = data_directory + '/binarizer.pickle'
+
+classifier_path = data_directory + '/classifier.pickle'
