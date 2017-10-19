db73e2cf05f16eaaea6d794e4abe9dfb9ec3da5a
Jan Beneš <janbenes1234@gmail.com>
2017-10-19 21:50:49 +0200

    Work in progress on classifying by paragraphs 3

--- classifying_by_paragraphs.py
+++ classifying_by_paragraphs.py
@@ -4,6 +4,7 @@ from sklearn.metrics import precision_recall_fscore_support as prfs
 
 import config
 from data_utils import load_pickle
+from precision_measurement import r_cut
 
 
 def build_topics_and_vectorized_paragraphs(raw_data_file_path, vectorizer, n_articles=1):
@@ -46,15 +47,19 @@ if __name__ == '__main__':
 
     articles, topics = build_topics_and_vectorized_paragraphs(config.testing_data_path, vectorizer, 2000)
 
-    article_topics = []
-    for article in articles:
-        y_rows = classifier.predict(article)
-        y_raw = np.sum(y_rows, 0)
-        article_topics.append(y_raw)
+    for i in [-0.5, -0.25, 0, 0.25, 0.5]:
+        print("Creating paragraph based predictions")
+        article_topics = []
+        for article in articles:
+            y_dec = classifier.decision_function(article)
+            y_rows = r_cut(y_dec, 1) + (y_dec > i)
+            y_raw = np.sum(y_rows, 0)
+            article_topics.append(y_raw)
 
-    y_pred_raw = np.array(article_topics)
-    y_pred = y_pred_raw > 0
-    y_true = binarizer.transform(topics)
+        print("Merging predictions to single array")
+        y_pred_raw = np.array(article_topics)
+        y_pred = y_pred_raw > 0
+        y_true = binarizer.transform(topics)
 
-    P, R, F, S = prfs(y_true, y_pred, average="samples")
-    print('F1 = %.3f (P = %.3f, R = %.3f)' % (F, P, R))
+        P, R, F, S = prfs(y_true, y_pred, average="samples")
+        print('i = %.2f, F1 = %.3f (P = %.3f, R = %.3f)' % (i, F, P, R))
