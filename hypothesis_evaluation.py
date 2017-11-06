f1c490f55c3ef2b80f159fddc51b2c6aabf1f7c2
Jan Beneš <janbenes1234@gmail.com>
2017-11-06 22:10:16 +0100

    Hypothesis evaluation 2

--- hypothesis_evaluation.py
+++ hypothesis_evaluation.py
@@ -8,39 +8,33 @@ if __name__ == '__main__':
     vectorizer = load_pickle(config.data_vectorizer_path)
     binarizer = load_pickle(config.topic_binarizer_path)
 
-    number_of_articles = 10
-    corpus, topics = build_corpus_and_topics(config.testing_data_path, number_of_articles)
-    corpus_paragraphs, topics_ = build_topics_and_paragraphs(config.testing_data_path, number_of_articles)
+    corpus, topics = build_corpus_and_topics(config.held_out_data_path)
+    corpus_paragraphs, topics_ = build_topics_and_paragraphs(config.held_out_data_path)
 
     x = vectorizer.transform(corpus)
     y = classifier.predict(x)
 
-    for i in range(0, len(y)):
+    number_of_articles = len(y)
+
+    fp_articles, fn_articles, fp_paragraphs, fn_paragraphs = 0., 0., 0., 0.
+    for i in range(0, number_of_articles):
+        print("Article index: " + str(i))
         y_transformed = set(binarizer.inverse_transform(y[i:i + 1, :])[0])
         current_topics = set(topics[i])
-        false_positives = y_transformed - current_topics
-        false_negatives = current_topics - y_transformed
-        if not false_positives or not false_negatives:
-            # Exclude perfect predictions
-            continue
 
-        print("\n-----------------\nActual: " + str(current_topics))
-        print("Predicted - full article: " + str(y_transformed))
         x_paragraphs = vectorizer.transform(corpus_paragraphs[i]['paragraphs'])
         y_transformed_paragraphs_list = binarizer.inverse_transform(classifier.predict(x_paragraphs))
-        print("Predicted - paragraphs: " + str(y_transformed_paragraphs_list) + "\n")
         y_transformed_paragraphs_set = set()
         y_transformed_paragraphs_set.update(*y_transformed_paragraphs_list)
 
-        print("False positives:")
-        for false_positive in false_positives:
-            print(false_positive + (" - IN" if false_positive in y_transformed_paragraphs_set else " - NOT in")
-                  + " paragraphs")
+        fp_articles += len(y_transformed - current_topics)
+        fn_articles += len(current_topics - y_transformed)
 
-        if not false_negatives:
-            continue
+        fp_paragraphs += len(y_transformed_paragraphs_set - current_topics)
+        fn_paragraphs += len(current_topics - y_transformed_paragraphs_set)
 
-        print("\nFalse negatives: ")
-        for false_negative in false_negatives:
-            print(false_negative + (" - IN" if false_negative in y_transformed_paragraphs_set else " - NOT in")
-                  + " paragraphs")
+    print("Full articles (per 1 classification) - false positives: %.2f, false negatives: %.2f" % (
+        fp_articles/number_of_articles, fn_articles/number_of_articles))
+
+    print("Per paragraphs (per 1 classification) - false positives: %.2f, false negatives: %.2f" % (
+        fp_paragraphs/number_of_articles, fn_paragraphs/number_of_articles))
