25e8884aa62f8b88dadfb9cb2fdb502faa5e7ca4
Jan Beneš <janbenes1234@gmail.com>
2018-01-02 22:01:17 +0100

    Measurements and optimal threshold discovery support for calibrated classifier

--- measure_precision.py
+++ measure_precision.py
@@ -2,21 +2,56 @@
 from sklearn.metrics import precision_recall_fscore_support as prfs
 
 from custom_imports import config
-from custom_imports.classifier_functions import predict_tuned
-from custom_imports.utils import load_pickle, build_corpus_and_topics
+from custom_imports.utils import load_pickle, build_corpus_and_topics, r_cut, yes_or_no
+
+
+def predict_tuned(x, classifier):
+    print('Classifying the data')
+    y_dec = classifier.decision_function(x)
+
+    # Ensures at least 1 predicted topic for each article
+    y_pred_min_topics = r_cut(y_dec, 1)
+
+    # Returns matrix where each elements is set to True if the element's value is bigger than threshold
+    y_pred_threshold = y_dec > -0.32  # -0.32 for 1 min topic (tested on held-out data)
+
+    return y_pred_min_topics + y_pred_threshold
+
+
+def predict_tuned_calibrated(x, classifier):
+    print('Classifying the data')
+    y_prob = classifier.predict_proba(x)
+
+    # Ensures at least 1 predicted topic for each article
+    y_pred_min_topics = r_cut(y_prob, 1)
+
+    # Returns matrix where each elements is set to True if the element's value is bigger than threshold
+    y_pred_threshold = y_prob > 0.3
+
+    return y_pred_min_topics + y_pred_threshold
+
 
 if __name__ == '__main__':
+    calibrated = yes_or_no("Do you want to use calibrated classifier?")
+
     vectorizer = load_pickle(config.data_vectorizer_path)
     binarizer = load_pickle(config.topic_binarizer_path)
 
     corpus, topics = build_corpus_and_topics(config.testing_data_path)
 
-    print("Transforming corpus by vectorizer")
+    print('Transforming corpus by vectorizer')
     x = vectorizer.transform(corpus)
-    print("Transforming article topics by binarizer")
+    print('Transforming article topics by binarizer')
     y_true = binarizer.transform(topics)
 
-    y_pred = predict_tuned(x)
+    if calibrated:
+        print('Loading calibrated classifier')
+        classifier = load_pickle(config.calibrated_classifier_path)
+        y_pred = predict_tuned_calibrated(x, classifier)
+    else:
+        print('Loading uncalibrated classifier')
+        classifier = load_pickle(config.classifier_path)
+        y_pred = predict_tuned(x, classifier)
 
-    P, R, F, S = prfs(y_true, y_pred, average="samples")
+    P, R, F, S = prfs(y_true, y_pred, average='samples')
     print('F1 = %.3f (P = %.3f, R = %.3f)' % (F, P, R))
--- optimal_threshold_discovery.py
+++ optimal_threshold_discovery.py
@@ -4,12 +4,13 @@ from matplotlib import pyplot, rc
 from sklearn.metrics import precision_recall_fscore_support as prfs
 
 from custom_imports import config
-from custom_imports.utils import load_pickle, build_corpus_and_topics, r_cut
+from custom_imports.utils import load_pickle, build_corpus_and_topics, r_cut, yes_or_no
 
 if __name__ == '__main__':
+    calibrated = yes_or_no("Do you want to use calibrated classifier?")
+
     vectorizer = load_pickle(config.data_vectorizer_path)
     binarizer = load_pickle(config.topic_binarizer_path)
-    classifier = load_pickle(config.classifier_path)
 
     corpus, topics = build_corpus_and_topics(config.held_out_data_path)
 
@@ -18,23 +19,31 @@ if __name__ == '__main__':
     print("Transforming article topics by binarizer")
     y_true = binarizer.transform(topics)
 
-    print("Classifying the data")
-    y_dec = classifier.decision_function(x)
+    if calibrated:
+        print('Loading calibrated classifier')
+        classifier = load_pickle(config.calibrated_classifier_path)
+        print("Classifying the data")
+        y_pred = classifier.predict_proba(x)
+        threshold_array = np.arange(0, 1.0, 0.01)
+    else:
+        print('Loading uncalibrated classifier')
+        classifier = load_pickle(config.classifier_path)
+        print("Classifying the data")
+        y_pred = classifier.decision_function(x)
+        threshold_array = np.arange(-1.0, 1.0, 0.01)
 
     # Ensures at least 1 predicted topic for each article
-    y_pred_min_topics = r_cut(y_dec, 1)
-
-    threshold_array = np.arange(-1.0, 1.0, 0.01)
+    y_pred_min_topics = r_cut(y_pred, 1)
 
     values = np.ones((threshold_array.shape[0], 4), dtype=np.float)
 
     for i, T in enumerate(threshold_array):
         # Returns matrix where each elements is set to True if the element's value is bigger than threshold
-        y_pred_T = y_dec > T
+        y_pred_T = y_pred > T
 
-        y_pred = y_pred_min_topics + y_pred_T
+        y_classifier = y_pred_min_topics + y_pred_T
 
-        P, R, F, S = prfs(y_true, y_pred, average="samples")
+        P, R, F, S = prfs(y_true, y_classifier, average="samples")
         values[i, :] = [T, F, P, R]
 
         print('threshold = %.2f, F1 = %.3f (P = %.3f, R = %.3f)' % (T, F, P, R))
