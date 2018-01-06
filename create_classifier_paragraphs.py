c9d5e976a0022ed9e2813f81bd25f5de53f6eab8
Jan Beneš <janbenes1234@gmail.com>
2018-01-06 10:41:04 +0100

    Work in progress on "index map" based approach

--- create_classifier_paragraphs.py
+++ create_classifier_paragraphs.py
@@ -1,22 +1,58 @@
 import scipy as sp
+from re import match
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
 
 from custom_imports import config
-from custom_imports.utils import build_topics_and_paragraphs, load_pickle, r_cut, save_pickle
+from custom_imports.utils import load_pickle, r_cut, save_pickle
 
-# TODO: adjust for calibrated classifier
+
+def build_topics_paragraphs_index_map(raw_data_file_path, n_articles=-1):
+    pattern = r'<article id="([0-9]+)" topics="(.*)">'
+
+    articles_paragraphs, topics, article_map = [], [], []
+    articles_processed, paragraph_index = 0, 0
+
+    with open(raw_data_file_path, 'r', encoding='utf-8') as handler:
+        for line in handler:
+            if line.startswith('<'):
+                if line == '</article>\n':
+                    article_map.append(paragraph_index - 1)
+                    # number of elements in article_map should be even since it contains start-end pairs
+                    assert len(article_map) % 2 == 0
+                    # End of article
+                    if articles_processed == n_articles:
+                        break
+                    continue
+
+                match_obj = match(pattern, line)
+                if match_obj:
+                    article_map.append(paragraph_index)
+                    assert len(article_map) % 2 == 1
+                    # First line of article
+                    topics.append(match_obj.group(2).split(' '))
+                    articles_processed += 1
+                    print(str(articles_processed) + ". article loaded")
+                    continue
+
+            paragraph_index += 1
+            articles_paragraphs.append(line)
+
+    if len(article_map) != (len(topics) * 2):
+        raise ValueError("Error: matrix dimensions do not match")
+
+    return articles_paragraphs, topics, article_map
+
 
 if __name__ == '__main__':
     classifier = load_pickle(config.classifier_path)
     vectorizer = load_pickle(config.data_vectorizer_path)
     binarizer = load_pickle(config.topic_binarizer_path)
 
-    articles, topics = build_topics_and_paragraphs(config.training_data_path)
-    inversed_topics = binarizer.transform(topics)
+    articles, topics, article_map = build_topics_paragraphs_index_map(config.training_data_path)
+    y_true = binarizer.transform(topics)
 
-    print("Removing binarizer from memory")
-    binarizer = None
+    # TODO: update the following code
 
     threshold = 0
 
@@ -34,7 +70,7 @@ if __name__ == '__main__':
         y_dec_article = classifier.decision_function(x_article)
 
         # set all the topics which were not in the original article to 0
-        y_article = inversed_topics[i] * y_dec_article
+        y_article = y_true[i] * y_dec_article
 
         x_list.append(x_article)
         y_list.append(y_article)
@@ -43,7 +79,7 @@ if __name__ == '__main__':
             print("{0} % done".format(i / one_percent))
 
     print("Removing unnecessary variables from memory")
-    vectorizer, classifier, corpus, topics, inversed_topics = None, None, None, None, None
+    vectorizer, classifier, corpus, topics, y_true = None, None, None, None, None
 
     print("Building training matrices")
     # create one matrix from the list of matrices
