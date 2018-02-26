714d9eb5159b3793ddcb7130e5203b25497d6ef2
Jan Beneš <janbenes1234@gmail.com>
2018-02-26 23:22:54 +0100

    Various approaches to LSTM training

--- segmentation/lstm/model_training_cosine.py
+++ segmentation/lstm/model_training_cosine.py
@@ -4,8 +4,11 @@ from keras.layers import LSTM, Dense, Dropout, TimeDistributed
 from keras.models import load_model
 from pathlib import Path
 
+from sklearn.metrics.pairwise import cosine_distances
+
 import config
-from utils import first_option
+from segmentation.distance_based_methods import compute_distance
+from utils import first_option, plot_thresholds
 
 
 def change_data_ratio(train, test, ratio=0.7, steps=200):
@@ -50,7 +53,7 @@ def build_model(_time_steps=200):
     _model = Sequential()
 
     # stateful=True means that the state is propagated to the next batch
-    _model.add(LSTM(32, input_shape=(_time_steps, 577), stateful=False, return_sequences=True))
+    _model.add(LSTM(32, input_shape=(_time_steps, 1), stateful=False, return_sequences=True))
     # _model.add(Dropout(0.5))
     _model.add(TimeDistributed(Dense(1, activation='sigmoid')))
     _model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -76,14 +79,13 @@ if __name__ == '__main__':
     [X_train, y_train, X_test, y_test] = change_data_ratio(config.get_seg_data('held_out'), config.get_seg_data('test'),
                                                            ratio=0.7, steps=time_steps)
 
+    print("Computing the distances")
+    X_train = compute_distance(X_train, cosine_distances)
+    X_test = compute_distance(X_test, cosine_distances)
+
     # Split the 2D matrix to 3D matrix of dimensions [samples, time_steps, features]
     X_train = split_to_time_steps(X_train)
     X_test = split_to_time_steps(X_test)
-
-    # Append 0 value to the beginning of y so the values represent if there was a boundary between current sample and
-    # the previous one, not the current and next
-    y_train = np.append(0, y_train)
-    y_test = np.append(0, y_test)
 
     # Split the 1D vector to 2D matrix of dimensions: [samples, time_steps]
     y_train = split_to_time_steps(y_train)
@@ -92,8 +94,12 @@ if __name__ == '__main__':
     y_train = np.reshape(y_train, (y_train.shape[0], y_train.shape[1], 1))
     y_test = np.reshape(y_test, (y_test.shape[0], y_test.shape[1], 1))
 
-    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, batch_size=400, shuffle=True)
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=1000, batch_size=400, shuffle=True)
 
     model.save(config.lstm_model)
 
     print(model.summary())
+
+    y_pred = model.predict(X_test)
+
+    plot_thresholds(y_test, y_pred, ensure_topic=False, average_type='samples')
