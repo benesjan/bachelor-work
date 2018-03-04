b6650f8ca4096b80fbb42c8e3f31accff7370b23
Jan Beneš <janbenes1234@gmail.com>
2018-03-04 22:19:14 +0100

    Segmentation of subtitles

--- segmentation/clustering/clustering_fixed.py
+++ segmentation/clustering/clustering_fixed.py
@@ -13,18 +13,18 @@ if __name__ == '__main__':
     data = config.get_seg_data('test')
 
     print("Loading the data")
-    y = np.load(data['y'])
+    X = np.load(data['y'])
     y_true = np.load(data['y_true_lm'])
 
     cluster_range = range(2, 6)
     y_pred = np.zeros((y_true.shape[0], 1))
     window_size = 20
-    one_percent = int(y.shape[0] / 100) if y.shape[0] > 100 else 1
+    one_percent = int(X.shape[0] / 100) if X.shape[0] > 100 else 1
 
     print("Clustering...")
-    for i in range(0, y.shape[0] - window_size, window_size):
+    for i in range(0, X.shape[0] - window_size, window_size):
         # for i in range(0, 40, window_size):
-        y_select = y[i:(i + window_size), :]
+        X_select = X[i:(i + window_size), :]
 
         # Initialize the clusterer with n_clusters value and a random generator
         # seed of 10 for reproducibility.
@@ -33,7 +33,7 @@ if __name__ == '__main__':
         else:
             clusterer = KMeans(n_clusters=n_clusters, random_state=10)
 
-        cluster_labels = clusterer.fit_predict(y_select)
+        cluster_labels = clusterer.fit_predict(X_select)
 
         for j in range(0, window_size - 1):
             if cluster_labels[j] != cluster_labels[j + 1]:
---
+++ subtitles_segmentation/data_processor.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+import numpy as np
+from keras.models import load_model
+from sklearn.metrics import precision_recall_fscore_support as prfs
+from sklearn.metrics.pairwise import cosine_distances
+
+import config
+from segmentation.distance_based_methods import compute_distance
+from segmentation.lstm.lstm_utils import split_to_time_steps
+from utils import load_pickle, first_option
+
+
+def process_data(path, window=10):
+    files = Path(path).glob('**/*.txt')
+    X, y = [], []
+
+    leftover = ''
+    for file_path in files:
+        article = leftover
+        with open(str(file_path), 'r', encoding='cp1250') as f:
+            for line in f:
+                article += line
+
+        # Split text to chunks
+        article_list = article.split()
+        while len(article_list) != 0:
+            list_selection = article_list[0:window]
+            if len(list_selection) == window:
+                X.append(" ".join(list_selection))
+                if leftover:
+                    y.append(1)
+                    leftover = ''
+                else:
+                    y.append(0)
+
+                article_list = article_list[window:]
+            else:
+                leftover = " ".join(article_list)
+                article_list = []
+
+        if not leftover:
+            y[-1] = 1
+
+    if leftover:
+        X.append(leftover)
+        y.append(1)
+
+    y = np.array(y)
+    return [X, y]
+
+
+if __name__ == '__main__':
+    [corpus, y_true] = process_data(config.subtitles_path, window=20)
+
+    vectorizer = load_pickle(config.vectorizer)
+
+    print('Transforming corpus by vectorizer')
+    corpus_tfidf = vectorizer.transform(corpus)
+
+    print('Loading the classifier')
+    classifier = load_pickle(config.classifier)
+
+    X = classifier.predict_proba(corpus_tfidf)
+
+    del corpus, corpus_tfidf, classifier, vectorizer
+
+    # LSTM part
+    if first_option('Do you want to use the model trained on cosine distances [c] or on raw SVM predictions [r]?',
+                    'c', 'r'):
+        print("Computing the distances")
+        X = compute_distance(X, cosine_distances)
+        model = load_model(config.lstm_model_1)
+        T = 0.46
+        y_true = y_true[1:]
+        assert len(X) == len(y_true), "Dimensions do not match"
+    else:
+        cosine = False
+        model = load_model(config.lstm_model_577)
+        T = 0.45
+
+    time_steps = model.get_config()[0]['config']['batch_input_shape'][1]
+
+    X = split_to_time_steps(X)
+    y_true = split_to_time_steps(y_true)
+
+    y_pred = model.predict(X) > T
+
+    P, R, F, S = prfs(y_true.flatten(), y_pred.flatten(), average='binary')
+    print('F1 = %.3f (P = %.3f, R = %.3f)' % (F, P, R))
